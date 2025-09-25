cca863a65ccdd8837c3a0352b984a62c4f9d64e5
Chap Carr <78283294+ChapCarr@users.noreply.github.com>
2025-09-24 19:29:55 -0500

    restart lol

--- main.c
+++ main.c
@@ -1,157 +1,36 @@
 #include "raylib.h"
+
 #include <string.h>
 #include <stdio.h>
 #include "shapes.h"
 
 #define SCREEN_WIDTH 500
 #define SCREEN_HEIGHT 800
-#define SQ_SIZE 40
-
-void initGrid(int (*grid)[20]){
-  for(int i = 0; i < 20; i++){
-    for(int j = 0; j < 10; j++){
-      grid[i][j] = 0;
-    }
-  }
-}
-
-int isValid(int x, int y, int (*grid)[20]){
-  // check walls  
- // if(x >= 9 || x < 0){
-   // return 0;
-  //}
-  // check bottom
-  if(y >= 20){
-    return 0;
-  }
-  
-  // check for shapes in grids 
-  if(grid[y][x] != 0){
-    return 0;
-  }
-
-  return 1;
-}
 
 
-void drawCurrent(int (*currentShape)[4], int x, int y, int (*grid)[20]){
-  int safeToDraw = 0;
-  for(int i = 0; i < 4; i++){
-    for(int j = 0; j < 4; j++){
-      if(currentShape[i][j] == 1){
-        int gridPosX = (x+j);
-        int gridPosY = (y+i); 
-        if(!isValid(gridPosX,gridPosY,grid)){
-          safeToDraw++;
-        }
-      }
-    }
-  }
-  if(safeToDraw == 0){
-    for(int i = 0; i < 4; i++){
-      for(int j = 0; j < 4; j++){
-        if(currentShape[i][j] != 0){
-          DrawRectangle((x+j)*40,(y+i)*40,SQ_SIZE,SQ_SIZE, BLUE);
-        }
-      }
-    }
-  }
-}
-
-void drawGrid(int (*grid)[20]){
-  for(int i = 0; i < 10; i++){
-    for(int j = 0; j < 20; j++){
-      if(grid[i][j] == 0){
-        DrawRectangleLines(i*SQ_SIZE,j*SQ_SIZE,SQ_SIZE,SQ_SIZE,RAYWHITE);
-      }          
-    }
-  }
-}
-
-void placeCurrentToGrid(int (*grid)[20], int (*curr)[4], int x, int y){
-  
-
-
-
-
-}
-
-int getWidth(int (*curr)[4]){
-  int width = 0;
-  for(int i = 0; i < 4; i++){
-    for(int j = 0; j < 4; j++){
-      if(curr[i][j] == 1){
-        if(j > width){
-          width = j;
-        }
-      }
-    }
-  }
-  return width;
-}
 
 
 
 int main(void)
-{
-   // const int screenWidth = 400;
-   // const int screenHeight = 800;
-    
-    const int squareSize = 40;
-    int grid[10][20];
-    int next_grid[10][20];
-//    int currentShape[4][4];
-    int currentX = 4;
-    int currentY= 0;
-    int clock = 0;
-    memset(grid,0,sizeof(grid)); 
+{   
     
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris");
 
   
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);  
 
-    // test block I piece
-    int currentShape[4][4] = {
-    {0, 1, 0, 0},
-    {0, 1, 0, 0},
-    {0, 1, 0, 0},
-    {0, 1, 0, 0}
-    };
 
-    initGrid(grid);
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())   
     {
-        // Update
-        // TODO: Update your variables here
-        clock++;
-        if(currentY < 18 & clock > 15){ // clock > 15 cets the speed that the blocks fall
-          printf("Inside of curY++ %d\n", currentY);
-          currentY++;
-          clock = 0; 
-        }
-        if(IsKeyDown(KEY_S)){
-          currentY++;
-        }
-        if(IsKeyDown(KEY_A) && currentX >=0){
-          currentX--;
-        }
-        if(IsKeyDown(KEY_D) && currentX+getWidth(currentShape)+1 < 10){
-
-          currentX++;
-        }
+        
 
         // Draw
-        BeginDrawing();
-        drawGrid(grid);
-        drawCurrent(currentShape,currentX,currentY,grid);
-        ClearBackground(BLACK);
-
-
+        BeginDrawing();  
+          ClearBackground(BLACK);
         EndDrawing();
     }
 
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();        
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,29 @@
-main:
-	gcc main.c shapes.c -o run -lraylib -lm -ldl -lpthread -lGL -lX11
-		
+# Makefile for Tetris with raylib
 
+CC = gcc
+SRC = main.c shapes.c
+OUT = run
+
+# Default flags
+CFLAGS = -std=c99 -Wall -Wextra -O2
+
+# Platform detection
+UNAME_S := $(shell uname -s)
+
+ifeq ($(UNAME_S), Darwin)  # macOS
+    # Homebrew installs raylib into /usr/local or /opt/homebrew
+    RAYLIB_PATH := $(shell brew --prefix raylib)
+    CFLAGS += -I$(RAYLIB_PATH)/include
+    LDFLAGS = -L$(RAYLIB_PATH)/lib -lraylib \
+              -framework Cocoa -framework IOKit -framework CoreVideo -framework OpenGL
+else  # Linux
+    LDFLAGS = -lraylib -lm -ldl -lpthread -lGL -lX11
+endif
+
+all: $(OUT)
+
+$(OUT): $(SRC)
+	$(CC) $(CFLAGS) $(SRC) -o $(OUT) $(LDFLAGS)
+
+clean:
+	rm -f $(OUT)
